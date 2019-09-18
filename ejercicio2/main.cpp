73aff425f90cbb11303df6deda2c0661ac9dc6a5
renzoromp <55298503+renzoromp@users.noreply.github.com>
2019-09-18 09:54:02 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-#include "Tipos.h"
 using namespace std;
 
-int main() {
-
-
-// ToDO
-
-  return 0;
+int main() { int zona; int claro;
+  cout << " - Ingrese la zona en la que se encuentra: ";
+  cin >> zona;
+  cout << " - Es usted usuario de Claro: SI(1) / NO(2) --> ";
+  cin >> claro;
+  cout << endl;
+  switch(zona){
+    case 1: cout << "  *Usted se encuentra ubicado en Super Vip!" << endl;
+            if(claro == 1){
+              cout << "  *El costo de su entrada es: " << 220 - (220 * 0.2);}
+            else
+              cout << "  *El costo de su entrada es: " << 220;          
+            break;
+    case 2: cout << "  *Usted se encuentra ubicado en Vip!" << endl;
+            if(claro == 1){
+              cout << "  *El costo de su entrada es: " << 170 - (170 * 0.2);}
+            else
+              cout << "  *El costo de su entrada es: " << 170;          
+            break;
+    case 3: cout << "  *Usted se encuentra ubicado en Preferencial!" << endl;
+            if(claro == 1){
+              cout << "  *El costo de su entrada es: " << 136 - (136 * 0.2);}
+            else
+              cout << "  *El costo de su entrada es: " << 136;          
+            break;
+    case 4: cout << "  *Usted se encuentra ubicado en General!" << endl;
+            if(claro == 1){
+              cout << "  *El costo de su entrada es: " << 59 - (59 * 0.2);}
+            else
+              cout << "  *El costo de su entrada es: " << 59;          
+            break;
+  }
 }
